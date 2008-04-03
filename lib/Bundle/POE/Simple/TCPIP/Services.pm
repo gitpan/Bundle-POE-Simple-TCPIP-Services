8c5c101efc76ef4c91c8acebe96fc62ea69f17ee
Chris Williams <chris@bingosnet.co.uk>
2008-04-03 11:59:14 +0000

    Import of BINGOS/Bundle-POE-Simple-TCPIP-Services-1.06 from CPAN.
    gitpan-cpan-distribution: Bundle-POE-Simple-TCPIP-Services
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         BINGOS/Bundle-POE-Simple-TCPIP-Services-1.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/Bundle/POE/Simple/TCPIP/Services.pm
+++ lib/Bundle/POE/Simple/TCPIP/Services.pm
@@ -1,9 +1,10 @@
 package Bundle::POE::Simple::TCPIP::Services;
 
 use strict;
+use warnings;
 use vars qw($VERSION);
 
-$VERSION = '1.04';
+$VERSION = '1.06';
 
 1;
 
@@ -30,19 +31,19 @@ Microsoft Windows terms "Simple TCP/IP Services", namely:
 
 =head1 CONTENTS
 
-POE 0.9999
+POE 1.0000
 
-POE::Component::Server::Echo 1.58
+POE::Component::Server::Echo 1.60
 
-POE::Component::Server::Chargen 1.08
+POE::Component::Server::Chargen 1.10
 
-POE::Component::Server::Discard 1.08
+POE::Component::Server::Discard 1.10
 
-POE::Component::Server::Daytime 1.08
+POE::Component::Server::Daytime 1.10
 
-POE::Component::Server::Qotd 1.08
+POE::Component::Server::Qotd 1.10
 
-POE::Component::Server::Time 1.08
+POE::Component::Server::Time 1.10
 
 =head1 AUTHOR
 
